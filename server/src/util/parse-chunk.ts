7bb8183c379afb7672c515134a41d12b83854ba2
Zach Garrett <zach@ketari.io>
2025-02-24 12:16:16 -0500

    remove unused utils

--- server/src/util/parse-chunk.ts
+++
@@ -1,9 +0,0 @@
-// Function to parse 32-bit integers from a buffer
-export function parseChunk(chunk: Buffer): number[] {
-  const numbers: number[] = [];
-  for (let i = 0; i < chunk.length; i += 4) {
-    const num = chunk.readInt32LE(i);
-    numbers.push(num);
-  }
-  return numbers;
-}
